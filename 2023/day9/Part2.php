28dce0ee9711cf229e7bf3b08ea64a5d918b38a1
jordanvanbeijnhem <jordanvanbeijnhem@gmail.com>
2023-12-16 09:42:47 +0100

    Add day 9

---
+++ 2023/day9/Part2.php
@@ -0,0 +1,34 @@
+<?php
+
+$lines = file(__DIR__ . "/input.txt");
+
+$nextValSum = 0;
+foreach ($lines as $line) {
+    $history = [
+        array_map(function ($item) {
+            return intval($item);
+        }, (explode(' ', rtrim($line))))
+    ];
+
+    $depth = 0;
+    $allZeros = false;
+    while (!$allZeros) {
+        $prev = [];
+        $allZeros = true;
+        for ($i = 0; $i < count($history[$depth]) - 1; $i++) {
+            $diff = $history[$depth][$i + 1] - $history[$depth][$i];
+            if ($diff !== 0) $allZeros = false;
+            $prev[] = $diff;
+        }
+        $history[] = $prev;
+        $depth++;
+    }
+
+    $nextVal = 0;
+    for ($i = count($history) - 2; $i >= 0; $i--) {
+        $nextVal = $history[$i][0] - $nextVal;
+    }
+    $nextValSum += $nextVal;
+}
+
+echo $nextValSum . PHP_EOL;
